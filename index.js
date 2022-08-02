65d37e2cd7375e4beace493da326fdfb2d3721e7
giabertu <giabertu99@gmail.com>
2022-08-02 11:50:41 +0100

    fix

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
+app.listen(/* PORT, () => {
   console.log(`Listening on  http://127.0.0.1:${PORT} 🍏`);
-});
+} */);
